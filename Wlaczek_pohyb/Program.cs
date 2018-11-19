33a997ccac803e61b30bb3fdd974ad2c87d5a9c5
Šimon <Radka@DESKTOP-TVP00LC>
2018-11-19 15:53:34 +0100

    Zde je pohyb vlaku.

--- Wlaczek_pohyb/Program.cs
+++ Wlaczek_pohyb/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Wlaczek_pohyb
@@ -10,6 +11,20 @@ namespace Wlaczek_pohyb
     {
         static void Main(string[] args)
         {
+            string wlaczek = "=>";
+            // string cil = "|°$ $°|";
+            int i = 0;
+
+            while (i < 22)
+            {
+                Thread.Sleep(1000);
+
+                Console.CursorVisible = false;
+                Console.SetCursorPosition(i++, 6);
+                Console.WriteLine(wlaczek);
+
+                Console.Clear();
+            }
         }
     }
 }
